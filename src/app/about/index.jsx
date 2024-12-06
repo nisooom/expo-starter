042139fecfbc9ec6e0f04d1fd83d35a3d0c8490b
nisooom <shahnishumbh55@gmail.com>
2024-12-06 11:20:53 +0530

    feat: jsx file support

--- src/app/about/index.jsx
+++ src/app/about/index.jsx
@@ -4,6 +4,8 @@ import { Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Page() {
+  
+  
   return (
     <View className="flex flex-1 items-center justify-center w-full h-full">
         <Text className="flex text-md">Hello World!</Text>

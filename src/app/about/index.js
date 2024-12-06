06b67f7ccc89cd8b7e6c71715f9af13de18a06b4
nisooom <shahnishumbh55@gmail.com>
2024-12-06 10:12:53 +0530

    init: basic expojs repo with expo router and tailwindcss

---
+++ src/app/about/index.js
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default function Page() {
+  return (
+    <View className="flex flex-1 items-center justify-center w-full h-full">
+        <Text className="flex text-md">Hello World!</Text>
+        <Link
+            className="text-md font-medium hover:underline web:underline-offset-4"
+            href="/"
+        >
+            Back
+        </Link>
+    </View>
+  );
+}
